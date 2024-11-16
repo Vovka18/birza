80ce16042db4ec2593f41964cb05d08fd0a5344d
Vovka18 <vova.artuhow@gmail.com>
2024-11-16 20:24:58 +0200

    dorabotka

--- my-app/src/pages/main/market/Market.jsx
+++ my-app/src/pages/main/market/Market.jsx
@@ -65,7 +65,7 @@ const Market = ({stateActive, setStateActive}) => {
     ]
 
   return (
-    <div className='Market'>
+    <div className={stateActive ? 'Market MarketActive' : 'Market'}>
         <div className="headline">
             <h3>Рынки</h3>
             <MarketClose onClick={()=>setStateActive(false)}/>
